b3520e1272a3c47e2d81f5dd6a47c29214b75a6f
leewonjae <105027084+iwjNitroCell@users.noreply.github.com>
2023-03-27 11:48:30 +0900

    this is movement func

--- moving_Func.cs
+++ moving_Func.cs
@@ -1,9 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player : MonoBehaviour
-{
     [SerializeField]
     private float speed; // 속도
     [SerializeField]
@@ -25,36 +19,15 @@ public class Player : MonoBehaviour
     private CapsuleCollider2D capsuleCol2D;
 
 
-    private void Awake()
+    // Execute inside the Awake function
     {
         rigid = GetComponent<Rigidbody2D>();
         capsuleCol2D = GetComponent<CapsuleCollider2D>();
-
     }
 
-    void Update()
-    {
-        IsStep();
-        Move();
-        TryJump();
-    }
-
-    private void FixedUpdate()
-    {
-        // 최대 속력 제한
-        if (rigid.velocity.x > maxSpeed)
-        {
-            //rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-            rigid.AddForce(new Vector2(-rigid.velocity.x * 0.5f, 0f));
-        }
-        else if (rigid.velocity.x < maxSpeed * (-1))
-        {
-            //rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
-            rigid.AddForce(new Vector2(-rigid.velocity.x * 0.5f, 0f));
-        }
-    }
-
-    private void IsStep()
+    
+    // Execute inside the Update function
+    private void IsStep() // 무언가 밟았는가를 감지하는 함수 
     {
         // 박스 형태로 충돌한 콜라이더 감지
         if (Physics2D.BoxCast(capsuleCol2D.bounds.center,    // Vector2 origin    -> 시작지점
@@ -63,7 +36,7 @@ public class Player : MonoBehaviour
             Vector2.down,                                   // vector2 direction -> 박스의 방향
             0.02f,                                          // float   distance  -> 박스의 최대 거리
                                                             // LayerMask -> 특정 레이어에서만 충돌 확인
-            layerMask = LayerMask.GetMask("NormalField") | LayerMask.GetMask("Enemy") | LayerMask.GetMask("iceField")))
+            layerMask = LayerMask.GetMask("")) // <- string 레이어 이름
         {
             if (rigid.velocity.y <= 0) // 떨어지고 있을 때
             {
@@ -76,8 +49,9 @@ public class Player : MonoBehaviour
             isGround = false;
         }
     }
-
-    private void Move()
+    
+    // Execute inside the Update function
+    private void Move() 
     {
         float moveDirX = Input.GetAxisRaw("Horizontal");
         if (moveDirX == 0f)
@@ -106,7 +80,23 @@ public class Player : MonoBehaviour
             rigid.AddForce(new Vector2(velocityX, 0f));
         }
     }
-
+    
+    // Execute inside the FixedUpdate function
+    private void LimitMaxSpeed() // 최대 속력 제한
+    {    
+        if (rigid.velocity.x > maxSpeed)
+        {
+            //rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            rigid.AddForce(new Vector2(-rigid.velocity.x * 0.5f, 0f));
+        }
+        else if (rigid.velocity.x < maxSpeed * (-1))
+        {
+            //rigid.velocity = new Vector2(-maxSpeed, rigid.velocity.y);
+            rigid.AddForce(new Vector2(-rigid.velocity.x * 0.5f, 0f));
+        }
+    }
+    
+    // Execute inside the Update function
     private void TryJump()
     {
         if (!Input.GetKeyDown(KeyCode.Space))
@@ -123,9 +113,8 @@ public class Player : MonoBehaviour
             Jump();
         } */
     }
-
     private void Jump()
     {
         rigid.velocity = new Vector2(rigid.velocity.x, jumpForce);
     }
-}
+
